8854fe1fb2bbd00b50d57b615ae37e3706929171
operossolistas <jonas.andriuskevicius@knf.stud.vu.lt>
2024-02-26 16:21:22 +0200

    Papildytas paskaitos kodas su daugyba ir dalyba. Kolkas nepatogus naudojimas, bet programa veikia.
    Sutvarkyti:
    1. Nedectina, kad ivestos raides, o ne skaicius.
    2. Pagerinti UI su gersniu interface.

--- calculatorvu/calculatorvu.py
+++ calculatorvu/calculatorvu.py
@@ -1,77 +1,68 @@
-''' 1 susidiegiame Flask
-        pip3 install Flask
-        pip install Flask
-''' 
+﻿from flask import Flask, request
 
-# 2. Importuojame
-
-from flask import Flask, request
 app = Flask(__name__)
 
+# Arithmetic operations
+def sudetis(pirmas, antras):
+    return pirmas + antras
 
+def atimtis(pirmas, antras):
+    return pirmas - antras
 
-skaicius = 0 # apsirasome kintamaji ( Globalus )
+def daugyba(pirmas, antras):
+    return pirmas * antras
 
-def sudetis(pirmas,antras):
-        return pirmas+antras
+def dalyba(pirmas, antras):
+    if antras == 0:  # Avoid division by zero
+        return "Dalyba iš nulio negalima"
+    return pirmas / antras
 
-
-
-
-@app.route("/") # Route 1
+@app.route("/")  # Route 1: Home/Form
 def hello_world():
+    return '''
+            <form action="/skaicius">
+                <label for="test">Skaicius 1:</label><br>
+                <input type="text" id="test" name="test" value="0"><br>
+                
+                <label for="test2">Skaicius 2:</label><br>
+                <input type="text" id="test2" name="test2" value="0"><br>
+                
+                <label for="operation">Operacija:</label><br>
+                <select name="operation" id="operation">
+                    <option value="sudetis">Sudėtis</option>
+                    <option value="atimtis">Atimtis</option>
+                    <option value="daugyba">Daugyba</option>
+                    <option value="dalyba">Dalyba</option>
+                </select><br><br>
+                
+                <input type="submit" value="Apskaičiuoti">
+            </form> 
+            '''
 
-    return f"""
-                <form action="/skaicius">
-                    <label for="test">skaicius 1</label><br>
-                        <input type="text" id="test" name="test" value="0"><br>
-                        </br></br>
-
-                    <label for="test2">skaicius 2</label><br>
-                        <input type="text" id="test2" name="test2" value="0"><br><br>
-                        </br></br>
-                        
-                    <label for="[[__ID__]]">skaicius 2</label><br>   
-                        <input type="text" id="[[__ID__]]" name="[[__ID__]]" value="0"><br><br>
-                        </br></br>
-
-                    <input type="submit" value="Submit">
-                </form> 
-            """
-
-@app.route("/labas")  # Route 2
+@app.route("/labas")  # Route 2: Greeting Counter
 def sakyk_labas():
-    global skaicius ## Naudoju globalu kintamaji
-    skaicius = skaicius +1 ## kaskart atidare pridedam 1
+    global skaicius  # Using global variable
+    skaicius = skaicius + 1  # Increment global variable by 1 each visit
     return f"Labas {skaicius}"
 
-
-    '''
-        /skaicius?test=100
-        /skaicius?test=0  &  test2=0
-    '''
-
-@app.route("/skaicius") # Route 3
+@app.route("/skaicius")  # Route 3: Calculation
 def skaiciavimo():
-    #UZKLAUSA. ARGUMENTAI. METODAS()
-    skaicius = request.args.get("test") ### Pasiimam argumenta is URL pvz.: /skaicius?test=100
-    skaicius2 = request.args.get("test2") ### Pasiimam argumenta 2 is URL pvz.: /skaicius?test2=100
+    skaicius1 = request.args.get("test", 0, type=int)
+    skaicius2 = request.args.get("test2", 0, type=int)
+    operation = request.args.get("operation")
 
-    suma = sudetis(int(skaicius2),int(skaicius))
+    if operation == "sudetis":
+        rezultatas = sudetis(skaicius1, skaicius2)
+    elif operation == "atimtis":
+        rezultatas = atimtis(skaicius1, skaicius2)
+    elif operation == "daugyba":
+        rezultatas = daugyba(skaicius1, skaicius2)
+    elif operation == "dalyba":
+        rezultatas = dalyba(skaicius1, skaicius2)
+    else:
+        return "Neteisinga operacija"
 
-    return f"Tavo ivestas skaicius: {suma}"
-
-
-
-
+    return f"Operacijos rezultatas: {rezultatas}"
 
 if __name__ == "__main__":
-    app.run()
-
-
-
-
-
-
-
-
+    app.run(debug=False, use_reloader=False)
